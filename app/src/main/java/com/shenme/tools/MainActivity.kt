54dcaaa2d8a70a41c6436db0bc97590612431305
shenme <ca_nc@qq.com>
2018-01-10 21:35:09 +0800

    'init'

---
+++ app/src/main/java/com/shenme/tools/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shenme.tools
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.shenme.tools.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
